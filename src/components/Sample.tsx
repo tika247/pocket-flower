ef72aa34d642df58ec9fcb8f66bc19fb7e8afab5
tika247 <taka7115web@gmail.com>
2024-07-14 16:44:44 +1200

    revise

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -4,12 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { useEffect } from 'react';
 
-export default function Sample() {
+export default async function Sample() {
+  const results = await fetch('/.netlify/functions/get_users').then((res) => res.json());
+  console.dir("results1")
+  console.dir(results)
+
   useEffect(() => {
     try {
       (async () => {
         const results = await fetch('/.netlify/functions/get_users').then((res) => res.json());
-        console.dir("results")
+        console.dir("results2")
         console.dir(results)
       })();
     } catch (err) {
@@ -18,6 +22,9 @@ export default function Sample() {
   }, []);
 
   return (
-    <p>Sample</p>
+    <div>
+      {results ? results.name : null}
+      <p>Sample</p>
+    </div>
   );
 }
