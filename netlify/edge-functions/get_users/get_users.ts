7d74ef4554102d2e94e8d4d6a8b715622daca98c
tika247 <taka7115web@gmail.com>
2024-07-14 13:56:13 +1200

    develop: setting for Netlify serverless functions

---
+++ netlify/edge-functions/get_users/get_users.ts
@@ -0,0 +1,29 @@
+// @ts-ignore
+// import type { Context } from 'https://edge.netlify.com'
+import {MongoClient} from 'mongodb'; 
+import dotenv from 'dotenv'
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI!);
+
+const clientPromise = mongoClient.connect();
+
+const handler = async (e: Event) => {
+  try {
+    const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+    const collection = database.collection(process.env.MONGODB_COLLECTION_USERS!);
+    const results = await collection.find({}).limit(10).toArray();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results)
+    }
+  } catch (err: any) {
+    return {statusCode: 500, body: err.toString()};
+  }
+}
+
+
+export default handler
+
+// export default async (request: Request, context: Context) =>
+//   Response.json({ hello: 'world', location: context.geo.city })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 // import Image from 'next/image';
 
+import { useCallback } from "react";
+
 export default function Home() {
+  try {
+    const results = useCallback(async () => {
+      await fetch("./netlify/edge-functions/get_users").then(res=>res.json());
+    }, []);
+    console.dir(results);
+  } catch (err) {
+    console.error(`Get users: ${err}`);
+  }
   return <main>Pocket-Flower</main>;
 }
