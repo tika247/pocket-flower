a3597d1411c7717957a65f6784f6477e20f37b43
tika247 <taka7115web@gmail.com>
2024-07-14 14:57:55 +1200

    revise

--- netlify/functions/get_users/get_users.js
+++ netlify/functions/get_users/get_users.js
@@ -17,7 +17,7 @@ const handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify(results),
-    };
+  }
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
   }
