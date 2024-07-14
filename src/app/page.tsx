5a11739b5ebd6a118c01e8420df514955d52895f
tika247 <taka7115web@gmail.com>
2024-07-14 15:07:15 +1200

    revise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 // import Image from 'next/image';
 
-export default async function Home() {
+export default function Home() {
+  console.dir("aa")
   try {
-    const results = await fetch('./netlify/functions/get_users/get_users').then((res) => res.json());
-    console.dir(results);
+    (async () => {
+      console.dir("bb")
+      const results = await fetch('./netlify/functions/get_users').then((res) => res.json());
+      console.dir(results);
+    })();
   } catch (err) {
+    console.dir("cc")
     console.error(`Get users: ${err}`);
   }
-  return <main>Pocket-Flower</main>;
+  return (
+    <main>
+      <h2>Pocket-Flower</h2>
+    </main>
+  );
 }
