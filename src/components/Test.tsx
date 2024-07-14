25e6d36b0f416cb9ffd32394778909c2e3f32bfc
tika247 <taka7115web@gmail.com>
2024-07-14 16:54:16 +1200

    revise

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -5,17 +5,13 @@ dotenv.config();
 import { useEffect } from 'react';
 
 export default function Test() {
-  useEffect(() => {
-    try {
-      (async () => {
-        const results = await fetch('/.netlify/functions/get_users').then((res) => res.json());
-        console.dir("results2")
-        console.dir(results)
-      })();
-    } catch (err) {
-      console.error(`Get users: ${err}`);
-    }
-  }, []);
+  (async () => {
+    const results = await fetch('/.netlify/functions/get_users').then((res) => res.json());
+    console.dir("results")
+    console.dir(results)
+  })();
+
+  console.dir("okok")
 
   return (
     <div>
