48c7376f7c7887dc6bda7e62eb08d641630f61e4
tika <>
2024-08-04 10:09:29 +1200

    refactor

--- netlify/functions/get_users/get_users_from_auth0.ts
+++ netlify/functions/get_users/get_users_from_auth0.ts
@@ -5,7 +5,6 @@ import connection from '@netlify/planetscale';
 export const handler: Handler = withAuth0(
   async (event: HandlerEvent, context: HandlerContext) => {
     const { rows: users } = await connection.execute('SELECT * FROM  users');
-
     return {
       statusCode: 200,
       body: JSON.stringify(users),
