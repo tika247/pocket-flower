5174faf926e36fa6881f8714aeb1425c3fbe111f
tika <>
2024-08-04 15:42:59 +1200

    test: auth0 + mongo

--- netlify/functions/auth_users/auth_users.ts
+++ netlify/functions/auth_users/auth_users.ts
@@ -1,20 +1,67 @@
+// import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
+// import { withAuth0 } from '@netlify/auth0';
+// import connection from '@netlify/planetscale';
+
+// export const handler: Handler = withAuth0(
+//   async (event: HandlerEvent, context: HandlerContext) => {
+//     // TODO: reference to users in MongoDB
+//     const { rows: users } = await connection.execute('SELECT * FROM  users');
+//     console.dir('auth_users');
+//     console.dir(users);
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(users),
+//     };
+//   },
+//   {
+//     auth0: {
+//       required: true,
+//     },
+//   },
+// );
+
+
+
+
+
+// TODO: reference to users in MongoDB
 import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
 import { withAuth0 } from '@netlify/auth0';
-import connection from '@netlify/planetscale';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI as string);
 
 export const handler: Handler = withAuth0(
   async (event: HandlerEvent, context: HandlerContext) => {
-    const { rows: users } = await connection.execute('SELECT * FROM  users');
-    console.dir('check');
-    console.dir(users);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(users),
-    };
+    try {
+      await mongoClient.connect();
+      const database = mongoClient.db(process.env.MONGODB_DATABASE);
+      const usersCollection = database.collection(process.env.MONGODB_COLLECTION_USERS as string);
+
+      const users = await usersCollection.find({}).toArray();
+      console.dir('auth_users');
+      console.dir(users);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(users),
+      };
+    } catch (error) {
+      console.error('Error connecting to MongoDB Atlas:', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'Internal Server Error' }),
+      };
+    } finally {
+      await mongoClient.close();
+    }
   },
   {
     auth0: {
       required: true,
     },
-  },
+  }
 );
